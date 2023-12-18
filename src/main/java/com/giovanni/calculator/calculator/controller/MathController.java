eb35cf6a1ff4d2013b825dc7d380ab8d7e074002
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2023-12-18 09:26:23 +0200

    first commit

---
+++ src/main/java/com/giovanni/calculator/calculator/controller/MathController.java
@@ -0,0 +1,35 @@
+package com.giovanni.calculator.calculator.controller;
+
+import com.giovanni.calculator.calculator.dto.DoMathRequest;
+import com.giovanni.calculator.calculator.exception.InvalidOperationException;
+import com.giovanni.calculator.calculator.service.MathOperator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/math")
+public class MathController {
+
+    private final MathOperator mathOperator;
+
+    @Autowired
+    public MathController(MathOperator mathOperator) {
+        this.mathOperator = mathOperator;
+    }
+
+    @PostMapping("/doMath")
+    public ResponseEntity<Map<String, Double>> doMath(@RequestBody DoMathRequest request) {
+        try {
+            double result = mathOperator.doMath(request.getOperand1(), request.getOperand2(), request.getOperation());
+            Map<String, Double> response = Collections.singletonMap("calcResponse", result);
+            return ResponseEntity.ok(response);
+        } catch (InvalidOperationException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Collections.emptyMap());
+        }
+    }
+}
---
+++ src/main/java/com/giovanni/calculator/calculator/dto/DoMathRequest.java
@@ -0,0 +1,13 @@
+package com.giovanni.calculator.calculator.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class DoMathRequest {
+
+    private double operand1;
+    private double operand2;
+    private String operation;
+}
